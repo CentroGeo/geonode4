ba3598ec8f5b2f24c9d976bb1973c08e0466c3a6
jdonlucas <jdonlucas93@gmail.com>
2024-08-15 11:46:20 +0200

    fix: problem with backport

--- geonode/people/api/serializers.py
+++ geonode/people/api/serializers.py
@@ -12,6 +12,27 @@ import geonode.base.api.serializers as base_serializers
 logger = logging.getLogger(__name__)
 
 
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        UserModel = get_user_model()
+        user = UserModel(username=self.validated_data['username'])
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        user.set_password(password)
+        user.save()
+        return user
+
 class UserSerializer(base_serializers.DynamicModelSerializer):
 
     link = base_serializers.AutoLinkField(read_only=True)
